000dd1c7ddce1aa90edd505384867522eafce676
andremartinez501 <andremartinez501@gmail.com>
2024-06-13 15:19:20 -0300

    mudanças

--- portfolio/lib/widgets/sections/habilidades.dart
+++ portfolio/lib/widgets/sections/habilidades.dart
@@ -16,101 +16,103 @@ class _HabilidadesState extends State<Habilidades> {
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final isDesktop = screenWidth > 800;
-    final viewportFraction = isDesktop ? 0.2 : 0.4;
-    final itemMargin = isDesktop ? 10.0 : 20.0;
-    final carouselHeight = isDesktop ? 300.0 : 200.0; // Convertendo para double
+    final viewportFraction = isDesktop ? 0.2 : 0.6; // Ajuste para telas menores
+    final itemMargin = isDesktop ? 10.0 : 5.0; // Margens menores para telas menores
+    final carouselHeight = isDesktop ? 300.0 : 250.0; // Ajuste de altura
 
-    return Column(
-      children: [
-        // Adicione o título Habilidades
-        const Text(
-          'Habilidades',
-          style: TextStyle(
-            fontSize: 36,
-            fontWeight: FontWeight.bold,
-            color: Color.fromRGBO(187, 187, 187, 1),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Adicione o título Habilidades
+          const Text(
+            'Habilidades',
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+              color: Color.fromRGBO(187, 187, 187, 1),
+            ),
           ),
-        ),
-        // Adicione um SizedBox para espaçamento
-        const SizedBox(height: 50), // Ajuste a altura conforme necessário
-        LayoutBuilder(
-          builder: (context, constraints) {
-            return CarouselSlider.builder(
-              itemCount: skillItems.length,
-              options: CarouselOptions(
-                height: carouselHeight,
-                viewportFraction: viewportFraction, // Ajuste para mostrar mais itens no desktop
-                enlargeCenterPage: false, // Desabilita o efeito de zoom no item central do carousel
-                enableInfiniteScroll: true, // Habilita rolagem infinita
-                autoPlay: true, // Habilita autoplay
-                autoPlayInterval: const Duration(seconds: 3), // Intervalo de 3 segundos
-                autoPlayAnimationDuration: const Duration(milliseconds: 800), // Duração da animação
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _current = index;
-                  });
-                },
-              ),
-              itemBuilder: (context, index, realIndex) {
-                final skill = skillItems[index];
-                final isCurrent = index == _current;
+          // Adicione um SizedBox para espaçamento
+          const SizedBox(height: 50), // Ajuste a altura conforme necessário
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return CarouselSlider.builder(
+                itemCount: skillItems.length,
+                options: CarouselOptions(
+                  height: carouselHeight,
+                  viewportFraction: viewportFraction, // Ajuste para mostrar mais itens no desktop
+                  enlargeCenterPage: false, // Desabilita o efeito de zoom no item central do carousel
+                  enableInfiniteScroll: true, // Habilita rolagem infinita
+                  autoPlay: true, // Habilita autoplay
+                  autoPlayInterval: const Duration(seconds: 3), // Intervalo de 3 segundos
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800), // Duração da animação
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                ),
+                itemBuilder: (context, index, realIndex) {
+                  final skill = skillItems[index];
+                  final isCurrent = index == _current;
 
-                // Verifique se as chaves existem e não são nulas
-                final String img = skill["img"] ?? '';
-                final String title = skill["title"] ?? 'No title';
+                  // Verifique se as chaves existem e não são nulas
+                  final String img = skill["img"] ?? '';
+                  final String title = skill["title"] ?? 'No title';
 
-                return AnimatedContainer(
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeInOut,
-                  margin: EdgeInsets.symmetric(horizontal: itemMargin),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: isCurrent ? const Color.fromRGBO(12, 6, 81, 1) : Colors.transparent,
-                    boxShadow: [
-                      if (isCurrent)
-                        const BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 10.0,
-                          offset: Offset(0, 4),
-                        ),
-                    ],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      AnimatedScale(
-                        scale: isCurrent ? 1.2 : 1.0,
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeInOut,
-                        child: Image.asset(
-                          img,
-                          height: isCurrent ? 100 : 80,
-                          width: isCurrent ? 100 : 80,
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      AnimatedOpacity(
-                        opacity: isCurrent ? 1.0 : 0.2, // Maior opacidade para itens adjacentes
-                        duration: const Duration(milliseconds: 500),
-                        curve: Curves.easeInOut,
-                        child: Text(
-                          title,
-                          style: TextStyle(
-                            fontSize: isCurrent ? 22 : 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                  return AnimatedContainer(
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut,
+                    margin: EdgeInsets.symmetric(horizontal: itemMargin),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: isCurrent ? const Color.fromRGBO(12, 6, 81, 1) : Colors.transparent,
+                      boxShadow: [
+                        if (isCurrent)
+                          const BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10.0,
+                            offset: Offset(0, 4),
+                          ),
+                      ],
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AnimatedScale(
+                          scale: isCurrent ? 1.2 : 1.0,
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                          child: Image.asset(
+                            img,
+                            height: isCurrent ? 100 : 80,
+                            width: isCurrent ? 100 : 80,
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 16),
-                    ],
-                  ),
-                );
-              },
-            );
-          },
-        ),
-      ],
+                        const SizedBox(height: 16),
+                        AnimatedOpacity(
+                          opacity: isCurrent ? 1.0 : 0.2, // Maior opacidade para itens adjacentes
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                          child: Text(
+                            title,
+                            style: TextStyle(
+                              fontSize: isCurrent ? 22 : 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- portfolio/lib/widgets/sections/main_mobile.dart
+++ portfolio/lib/widgets/sections/main_mobile.dart
@@ -8,64 +8,61 @@ class MainMobile extends StatelessWidget {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
     final screenHeight = screenSize.height;
-    return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
-                  height: screenHeight,
-                  constraints: const BoxConstraints(minHeight: 560.0,),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      
-                      //Imagem
-                      Image.asset(
-                        "assets/prompt.png",
-                        width: screenWidth,),
-                      const SizedBox(height: 40,),
-                      //Texto
-                      RichText(
-                            text: const TextSpan(
-                              text: "Olá, meu nome é",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 15,
-                                color: Color.fromRGBO(216,45,45,1)
-                              ),
-                              children: [
-                                TextSpan(
-                                  text:"\nAndré Martinez de Souza.",
-                                  style: TextStyle(
-                                    height: 1.5,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 35,
-                                    color: Color.fromRGBO(187, 187, 187, 1)
-                                  ) 
-                                ),
-                                TextSpan(
-                                  text: "\nE este é meu portfólio:",
-                                  style: TextStyle(
-                                    height: 1.5,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 35,
-                                    color: Color.fromRGBO(91, 91, 91, 1)
-                                    )
-                                  ),
-                                TextSpan(
-                                  text: """\nSou um estudante de Ciências da Computação atualmente no terceiro semestre no Instituto Mauá de Tecnologia.\nTenho interesse pela área de Ciência de Dados, buscando diariamente meu desenvolvimento profissional e pessoal,\nme desafiando com novos projetos e linguagens.""",
-                                  style: TextStyle(
-                                    height: 1.5,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 15,
-                                    color: Color.fromRGBO(91, 91, 91, 1)
-                                    )
-                                  ),
-                              ]
-                            ),
-                          ),
 
-                      
-                    ],
+    return SingleChildScrollView(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 40.0, vertical: 30.0),
+        constraints: BoxConstraints(minHeight: screenHeight),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Imagem
+            Image.asset(
+              "assets/prompt.png",
+              width: screenWidth,
+            ),
+            const SizedBox(height: 40),
+            // Texto
+            RichText(
+              text: const TextSpan(
+                text: "Olá, meu nome é",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15,
+                    color: Color.fromRGBO(216, 45, 45, 1)),
+                children: [
+                  TextSpan(
+                    text: "\nAndré Martinez de Souza.",
+                    style: TextStyle(
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                        color: Color.fromRGBO(187, 187, 187, 1)),
                   ),
-                );
+                  TextSpan(
+                    text: "\nE este é meu portfólio:",
+                    style: TextStyle(
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                        color: Color.fromRGBO(91, 91, 91, 1)),
+                  ),
+                  TextSpan(
+                    text:
+                        """\nSou um estudante de Ciências da Computação atualmente no terceiro semestre no Instituto Mauá de Tecnologia.\nTenho interesse pela área de Ciência de Dados, buscando diariamente meu desenvolvimento profissional e pessoal,\nme desafiando com novos projetos e linguagens.""",
+                    style: TextStyle(
+                        height: 1.5,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Color.fromRGBO(91, 91, 91, 1)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
